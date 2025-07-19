1df8e865a751a1c1e4e6e133adcd2c5d9ddb6867
Aneesh <aneeshsharmag@gmail.com>
2025-07-19 19:23:54 +0530

    experience done

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,23 +12,23 @@ const Experience: React.FC = () => {
         <div className="flex justify-between flex-col sm:flex-row text-neutral-300">
           <div>
             <h3 className="text-xl xl:text-2xl font-bold text-white">
-              Larsen & Toubro Defence
+              VISWAM.AI
             </h3>
-            <p className="sm:text-lg italic">ML Intern</p>
+            <p className="sm:text-lg italic">AI Developer Intern</p>
           </div>
           <div>
             <p className="sm:text-right text-base xl:text-lg">
-              March 2024 – Sep 2024
+              May 2025 – June 2025
             </p>
           </div>
         </div>
 
         <p className="mt-4 text-base xl:text-lg font-medium">
-          Developed GAN models for synthetic data generation, optimized
-          CentreNet for real-time PCB analysis, and implemented denoising
-          algorithms in Python and C++. Gained expertise in synthetic data,
-          computer vision, and real-time algorithm optimization for defense
-          applications.
+          Interned at the prestigious Summer of AI 2025 (SoAI) internship program by VISWAM.AI, in collaboration with 
+          IIIT Hyderabad — one of India's most esteemed research institutions. Gained hands-on experience in AI and 
+          machine learning through real-world projects involving Python, DevOps, and collaborative software practices. 
+          Worked on locally sourced data, fine-tuned AI models, and contributed to impactful applications within an 
+          open-source and innovation-driven ecosystem.
         </p>
       </WobbleCard>
     </div>
